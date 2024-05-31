11dee85d4ee2ee0da4359ec45f5c1c825f489f71
速水健杜 <hayami527kento@gmail.com>
2024-05-31 14:17:12 +0900

    create repotimelapse

---
+++ repotimelapse/repository.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""""RepoTimelapse：GitHubのリポジトリURLからリポジトリの成長過程を動画化する。"""
+__author__ = 'Hayami Kento'
+__version__ = '0.1.0'
+__date__ = '2024/05/31 (created: 2024/05/31)'
+
+class Repository:
+    def main(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
