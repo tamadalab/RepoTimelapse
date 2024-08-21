0777646acd7020389ae49d277ca326076a789872
速水健杜 <hayami527kento@gmail.com>
2024-08-21 23:08:44 +0900

    treemap generator

---
+++ src/treemap_generator.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import plotly.express as px
+
+
+class TreeMapGenerator:
+    @staticmethod
+    def generate_treemap(df, output_path, path_columns, title="Example Treemap"):
+        fig = px.treemap(
+            df,
+            path=path_columns,  # 動的に作成したパスカラムのリストを使用
+            values="size",
+            color="date",
+            hover_data=["date", "lines"],
+            title=title
+        )
+        fig.write_html(output_path)
